1b6419d0ebe317f9ae297ffae343e18f11f05f09
MrGeoTech <mrgeotech@mrgeotech.net>
2024-12-08 17:45:09 -0600

    Update webserver

--- hosts/www/nginx/isaacgeorge.net.nix
+++ hosts/www/nginx/isaacgeorge.net.nix
@@ -39,7 +39,7 @@ in {
                             try_files $fastcgi_script_name =404;
                             include ${config.services.nginx.package}/conf/fastcgi_params;
                             fastcgi_split_path_info  ^(.+\.php)(.*)$;
-                            fastcgi_pass unix:${config.services.phpfpm.pools.${user}.socket};
+                            fastcgi_pass unix:${config.services.phpfpm.pools.${appDomain}.socket};
                             fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
                             fastcgi_param  PATH_INFO        $fastcgi_path_info;
 
