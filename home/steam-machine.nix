ca55a5740097d4704856c99be305801d631637ad
Isaac George <git-mail@isaacgeorge.net>
2025-09-02 20:53:10 -0500

    Fix home manager config

--- home/steam-machine.nix
+++ home/steam-machine.nix
@@ -1,5 +1,20 @@
 {
+  config,
+  outputs,
+  ...
+}: {
   imports = [
     ./common/core
+    ./common/optional/desktops/hyprland
+    ./common/optional/desktops/waybar
+    ./common/optional/desktops/gtk.nix
+    ./common/optional/desktops/qt5.nix
+    ./common/optional/desktops/xdg.nix
   ];
+
+  home = {
+    username = "mrgeotech";
+    homeDirectory = "/home/${config.home.username}";
+    stateVersion = "24.11";
+  };
 }
