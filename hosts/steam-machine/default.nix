20ee425ad795a49fc028431140c6568a549e96a3
Isaac George <git-mail@isaacgeorge.net>
2025-09-09 23:50:14 -0500

    Update formatting and auto login

--- hosts/steam-machine/default.nix
+++ hosts/steam-machine/default.nix
@@ -1,97 +1,96 @@
 {
-    pkgs,
-    config,
-    inputs,
-    outputs,
-    ...
+  pkgs,
+  config,
+  inputs,
+  outputs,
+  ...
 }: {
-    imports =
-        [
-        # Hardware config
-        inputs.hardware.nixosModules.common-cpu-amd
-        inputs.hardware.nixosModules.common-gpu-nvidia
-        ./hardware-configuration.nix
+  imports = [
+    # Hardware config
+    inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-gpu-nvidia
+    ./hardware-configuration.nix
 
-        # Common config
-        ../common/core
+    # Common config
+    ../common/core
+      # Optional configs
+    ../common/optional/fonts.nix
+    ../common/optional/hyprland.nix
+    ../common/optional/pipewire.nix
+    ../common/optional/steam.nix
 
-        # Optional configs
-        ../common/optional/fonts.nix
-        ../common/optional/hyprland.nix
-        ../common/optional/pipewire.nix
-        ../common/optional/steam.nix
+    # User config
+    ../common/users/mrgeotech
+    ../common/users/mrgeotech/optional.nix
+  ];
 
-        # User config
-        ../common/users/mrgeotech
-        ../common/users/mrgeotech/optional.nix
-    ];
+  # Bootloader (uses grub instead of systemd-boot)
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+    configurationLimit = 5;
+  };
 
-    # Bootloader (uses grub instead of systemd-boot)
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-    boot.loader.grub = {
+  networking = {
+    networkmanager.wifi.powersave = true;
+    hostName = "steam-machine";
+    useNetworkd = true;
+    interfaces.wlp5s0.useDHCP = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    hyprpolkitagent
+  ];
+
+  security.polkit.enable = true;
+
+  services = {
+    printing.enable = true;
+
+    blueman.enable = true;
+    gnome.gnome-keyring.enable = true;
+
+    udev.extraRules = ''
+      SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE="0666"
+      '';
+
+    getty.autologinUser = "mrgeotech";
+    displayManager.autoLogin = {
       enable = true;
-      device = "/dev/sda";
-      useOSProber = true;
-      configurationLimit = 5;
+      user = "mrgeotech";
     };
+  };
 
-    networking = {
-        networkmanager.wifi.powersave = true;
-        hostName = "steam-machine";
-        useNetworkd = true;
-        interfaces.wlp5s0.useDHCP = true;
-    };
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
-    environment.systemPackages = with pkgs; [
-        hyprpolkitagent
-    ];
+  # Load nvidia driver for Xorg and Wayland
+  console.useXkbConfig = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 
-    security.polkit.enable = true;
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["nvidia"];
+    xkb.options = "ctrl:nocaps";
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
 
-    services = {
-        printing.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidiaBusId = "PCI:1:00:0";
+  };
 
-        blueman.enable = true;
-        gnome.gnome-keyring.enable = true;
-
-        udev.extraRules = ''
-            SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE="0666"
-        '';
-
-        getty.autologinUser = "mrgeotech";
-    };
-
-    hardware.graphics = {
-        enable = true;
-        enable32Bit = true;
-    };
-
-    # Load nvidia driver for Xorg and Wayland
-    console.useXkbConfig = true;
-    hardware.bluetooth.enable = true;
-    hardware.bluetooth.powerOnBoot = true;
-
-    # Configure keymap in X11
-    services.xserver = {
-        enable = true;
-        videoDrivers = ["nvidia"];
-        xkb.options = "ctrl:nocaps";
-        xkb.layout = "us";
-        xkb.variant = "";
-    };
-
-    hardware.nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = true;
-        powerManagement.finegrained = false;
-        open = true;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
-        prime = {
-            intelBusId = "PCI:0:02:0";
-            nvidiaBusId = "PCI:1:00:0";
-        };
-    };
-
-    system.stateVersion = "24.05";
+  system.stateVersion = "24.05";
 }
