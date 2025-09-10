e253628be0c975a10e3bec68c633a12a9ec6d7b5
Isaac George <git-mail@isaacgeorge.net>
2025-09-09 21:30:46 -0500

    Remove efi support

--- hosts/steam-machine/default.nix
+++ hosts/steam-machine/default.nix
@@ -25,15 +25,11 @@
 
     # Bootloader (uses grub instead of systemd-boot)
     boot.kernelPackages = pkgs.linuxPackages_latest;
-    boot.loader = {
-      grub = {
-        enable = true;
-        device = "/dev/sda";
-        efiSupport = true;
-        useOSProber = true;
-        configurationLimit = 5;
-      };
-      efi.canTouchEfiVariables = true;
+    boot.loader.grub = {
+      enable = true;
+      device = "/dev/sda";
+      useOSProber = true;
+      configurationLimit = 5;
     };
 
     networking = {
