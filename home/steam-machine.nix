a3af95946359adc73bcc9ceb2da5541261e136ef
Isaac George <git-mail@isaacgeorge.net>
2025-09-13 17:25:34 -0500

    Add protonup

--- home/steam-machine.nix
+++ home/steam-machine.nix
@@ -18,5 +18,12 @@
     username = "mrgeotech";
     homeDirectory = "/home/${config.home.username}";
     stateVersion = "24.11";
+    packages = with pkgs; [
+      protonup
+    ];
+ 
+    sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
+    };
   };
 }
