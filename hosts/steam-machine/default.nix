4a169061eabe3ed6e73b7404427ae93411ba0fa5
Isaac George <git-mail@isaacgeorge.net>
2025-09-13 17:20:20 -0500

    Start optimizing for gaming

--- hosts/steam-machine/default.nix
+++ hosts/steam-machine/default.nix
@@ -14,7 +14,6 @@
       # Optional configs
     ../common/optional/fonts.nix
     ../common/optional/pipewire.nix
-    ../common/optional/steam.nix
 
     # User config
     ../common/users/mrgeotech
@@ -40,6 +39,16 @@
   environment.systemPackages = with pkgs; [
     hyprpolkitagent
   ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
 
   security.polkit.enable = true;
 
