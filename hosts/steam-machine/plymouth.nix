9a055b71804e1098fba06c1f69f61f61605d9beb
Isaac George <git-mail@isaacgeorge.net>
2025-09-18 23:14:07 -0500

    Switch to different theme

--- hosts/steam-machine/plymouth.nix
+++ hosts/steam-machine/plymouth.nix
@@ -1,24 +1,8 @@
 { config, pkgs, ... }:
-
-let
-  steamosTheme = pkgs.stdenv.mkDerivation {
-    pname = "plymouth-theme-steamos";
-    version = "0.17+bsos2";
-    src = pkgs.fetchurl {
-      url = "https://repo.steampowered.com/steamos/pool/main/p/plymouth-themes-steamos/plymouth-themes-steamos_0.17.tar.xz";
-      sha256 = "sha256-RLMJYlVSPzMsSe5LtWdWBbsuk1ZejQ73JUp6LPYNjlM=";
-    };
-
-    installPhase = ''
-      mkdir -p $out/share/plymouth/themes
-      cp -r steamos-* $out/share/plymouth/themes/
-    '';
-  };
-in
 {
   boot.plymouth = {
     enable = true;
-    theme = "steamos";
-    themePackages = [ steamosTheme ];
+    theme = "loader_alt";
+    themePackages = [ pkgs.adi1090x-plymouth-themes ];
   };
 }
